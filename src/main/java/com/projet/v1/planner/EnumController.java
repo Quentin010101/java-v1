ff5699dd2be52e9c80a5d601f476985fd8628d83
qcozic <q_cozic@hotmail.fr>
2024-03-18 16:45:19 +0100

    test

--- src/main/java/com/projet/v1/planner/EnumController.java
+++ src/main/java/com/projet/v1/planner/EnumController.java
@@ -2,6 +2,7 @@ package com.projet.v1.planner;
 
 import com.projet.v1.dto.ResponseDto;
 import com.projet.v1.dto.ResponseObjectDto;
+import com.projet.v1.planner.dto.Choice;
 import com.projet.v1.planner.enumeration.Importance;
 import com.projet.v1.planner.enumeration.Progression;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,20 +21,20 @@ import java.util.Map;
 public class EnumController {
 
     @GetMapping("/importance")
-    public ResponseObjectDto<Map<Integer, String>> getPriority(){
-        Map<Integer, String> map = new HashMap<>();
+    public ResponseObjectDto<List<Choice>> getPriority(){
+        List<Choice> liste = new ArrayList<>();
         for(Importance imp : Importance.values()){
-            map.put(imp.getId(), imp.getText());
+            liste.add(new Choice(imp.getId(), imp.getText()));
         }
-        return new ResponseObjectDto<>(new ResponseDto("", true), map);
+        return new ResponseObjectDto<>(new ResponseDto("", true), liste);
     }
 
     @GetMapping("/progression")
-    public ResponseObjectDto<Map<Integer, String>> getProgression(){
-        Map<Integer, String> map = new HashMap<>();
+    public ResponseObjectDto<List<Choice>> getProgression(){
+        List<Choice> liste = new ArrayList<>();
         for(Progression imp : Progression.values()){
-            map.put(imp.getId(), imp.getText());
+            liste.add(new Choice(imp.getId(), imp.getText()));
         }
-        return new ResponseObjectDto<>(new ResponseDto("", true), map);
+        return new ResponseObjectDto<>(new ResponseDto("", true), liste);
     }
 }
---
+++ src/main/java/com/projet/v1/planner/dto/Choice.java
@@ -0,0 +1,15 @@
+package com.projet.v1.planner.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Choice {
+    private Integer id;
+    private String text;
+
+
+}
